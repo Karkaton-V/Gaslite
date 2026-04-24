cacbf7ea6a9b85629bd5bc6bb2d218befec77a69
ZeRoCodesBZ <114606234+ZeRoCodesBZ@users.noreply.github.com>
2026-04-24 15:41:21 -0500

    Update button-group.tsx

--- src/shared/ui/button-group.tsx
+++ src/shared/ui/button-group.tsx
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from "class-variance-authority"
 import { Slot } from "radix-ui"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/shared/lib/utils/index"
 import { Separator } from "@/shared/ui/separator"
 
 const buttonGroupVariants = cva(
