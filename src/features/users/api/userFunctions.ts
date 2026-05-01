8715f358bf66ebd499a9eb12fda8fa5ec6afe41f
tbraud03 <128174373+tbraud03@users.noreply.github.com>
2026-05-01 15:53:29 -0500

    add userfunctions.ts

---
+++ src/features/users/api/userFunctions.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@/shared/lib/supabase/client";
+
+export async function updateDisplayName(){
+
+}
+
+export async function updateHandlle(){
+
+}
+
+export async function updateAvatar(){
+
+}
